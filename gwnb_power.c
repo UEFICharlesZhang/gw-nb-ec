3c091fe252eaf45f6231f2df2346ad6f0d06498a
Zhang Shuzhen <zhangshuzhen@live.cn>
2022-08-25 14:00:10 +0800

    optimize code.

--- gwnb_power.c
+++ gwnb_power.c
@@ -28,39 +28,6 @@ struct gw_nb_battery_data {
 	struct power_supply *ac;
 };
 
-#define BIT0		    0x00000001
-#define BIT1		    0x00000002
-#define BIT2		    0x00000004
-#define BIT3		    0x00000008
-#define BIT4		    0x00000010
-#define BIT5		    0x00000020
-#define BIT6		    0x00000040
-#define BIT7		    0x00000080
-#define BIT8		    0x00000100
-#define BIT9		    0x00000200
-#define BIT10		   0x00000400
-#define BIT11		   0x00000800
-#define BIT12		   0x00001000
-#define BIT13		   0x00002000
-#define BIT14		   0x00004000
-#define BIT15		   0x00008000
-#define BIT16		   0x00010000
-#define BIT17		   0x00020000
-#define BIT18		   0x00040000
-#define BIT19		   0x00080000
-#define BIT20		   0x00100000
-#define BIT21		   0x00200000
-#define BIT22		   0x00400000
-#define BIT23		   0x00800000
-#define BIT24		   0x01000000
-#define BIT25		   0x02000000
-#define BIT26		   0x04000000
-#define BIT27		   0x08000000
-#define BIT28		   0x10000000
-#define BIT29		   0x20000000
-#define BIT30		   0x40000000
-#define BIT31		   0x80000000
-
 #define POWER_STATUS 0xB0
 #define AC_STATUS 1U << 0
 #define BAT_STATUS 1U << 1
@@ -204,10 +171,9 @@ EcReadMem (
 static int gw_ec_read(int offset)
 {
 	int tmp;
-	printk(KERN_ERR "gw_ec_read\n");
+	// printk(KERN_ERR "gw_ec_read\n");
 	EcReadMem(offset,&tmp);
-	printk(KERN_ERR "gw_ec_read value:0x%x\n",tmp);
-
+	// printk(KERN_ERR "gw_ec_read value:0x%x\n",tmp);
 	return tmp;
 }
 
@@ -256,12 +222,12 @@ static int goldfish_battery_get_property(struct power_supply *psy,
 	switch (psp) {
 	case POWER_SUPPLY_PROP_STATUS:
 		temp = gw_ec_read(BATTERY_STATUS);
-		if (temp & BIT6) {
+		if (temp & BIT(6)) {
 			val->intval = POWER_SUPPLY_STATUS_DISCHARGING;
 		} else {
 			val->intval = POWER_SUPPLY_STATUS_CHARGING;
 		}
-		if (temp & BIT5) {
+		if (temp & BIT(5)) {
 			val->intval = POWER_SUPPLY_STATUS_FULL;
 		}
 		break;
